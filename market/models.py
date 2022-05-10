220ca28e66f7b713dc5c2a1aa61dbbd688b2b6b4
beth nduta <bethnduta05@gmail.com>
2022-05-10 18:20:50 +0300

    deployment

--- market/models.py
+++ market/models.py
@@ -10,6 +10,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username  =db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    
     password_hash = db.Column(db.String(length=60), nullable=False)
     # Pitches = db.relationship('pitch', backref='owned_user', lazy=True)
 
